635435c1fbf93c3304353cb17a14f2fb1804fc0c
unknown <aladynjr@gmail.com>
2024-11-27 00:39:36 +0100

    make headless

--- python/main.py
+++ python/main.py
@@ -309,8 +309,8 @@ async def main():
             
             # Create the token extractor with our event handler
             extractor = ExtractorClass(
-                tabs_per_browser=1,
-                headless=False,
+                tabs_per_browser=4,
+                headless=True,
                 on_token_found=event_manager.on_token_found
             )
             
