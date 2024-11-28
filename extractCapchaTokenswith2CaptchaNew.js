05e3be718d7715db5930a368edbbbc4f5855fb6f
unknown <aladynjr@gmail.com>
2024-11-28 02:13:17 +0100

    increase batch size

--- extractCapchaTokenswith2CaptchaNew.js
+++ extractCapchaTokenswith2CaptchaNew.js
@@ -16,8 +16,8 @@ const osPlatform = os.platform();
                 
 const executablePath = osPlatform.startsWith('win')  ? "C://Program Files//Google//Chrome//Application//chrome.exe" : "/usr/bin/google-chrome";
 
-const CONCURRENT_BROWSERS = 6;
-const BATCH_SIZE = 12;
+const CONCURRENT_BROWSERS = 8;
+const BATCH_SIZE = 16;
 
 const ALLOW_PROXY = false;
 
