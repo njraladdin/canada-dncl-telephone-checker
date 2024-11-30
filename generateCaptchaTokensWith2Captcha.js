c2d5633bccce632d6935d3ab039e4b0dafc4b8d7
unknown <aladynjr@gmail.com>
2024-11-30 01:10:44 +0100

    increase browsers

--- generateCaptchaTokensWith2Captcha.js
+++ generateCaptchaTokensWith2Captcha.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // Configuration
 const CONCURRENT_BROWSERS = 6;
-const TABS_PER_BROWSER = 2;
+const TABS_PER_BROWSER = 4;
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36';
 const APIKEY = process.env['2CAPTCHA_API_KEY'];
 const ALLOW_PROXY = false;
